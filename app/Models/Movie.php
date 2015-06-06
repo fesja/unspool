f77020d23b49e3080b92d4cec93cd36741ac6450
Javier F. Escribano <javier@touristeye.com>
2015-06-07 00:04:05 +0200

    return imdb_rating and rt_meter as decimal and int

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -9,6 +9,17 @@ class Movie extends Model {
     protected $visible = ['id', 'title', 'year', 'rating', 'runtime', 'genre', 'released', 'director', 'cast',
         'imdb_rating', 'poster', 'full_plot', 'awards', 'rt_meter',  'created_at'];
 
+    /**
+     * The attributes that should be casted to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'year'        => 'int',
+        'imdb_rating' => 'double',
+        'rt_meter'    => 'int'
+    ];
+
     public function users()
     {
         return $this->belongsToMany('App\Models\User', 'wishes');
